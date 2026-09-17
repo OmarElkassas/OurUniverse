1d2de388784722438b8a9a483e373f49184e9017
Omar Elkassas <65733605+OmarElkassas@users.noreply.github.com>
2026-09-17 17:23:45 +0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: 'https://mkgcjbnqozlykqravdnl.supabase.co',
-  SUPABASE_KEY: 'sb_publishable_sdbaYQQHqnUFTa8qj7vJeQ_AyhmZsgH',
-  RELATIONSHIP_START: '2025-12-19',
-  NEXT_MEET_DATE: '2027-02-10T18:00:00+02:00'
-};
